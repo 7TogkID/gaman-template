6d82d0cd1eeccb67f4afe281381f0fbc45cf514c
Angga7Togk <kiplihode123321@gmail.com>
2025-06-18 11:27:27 +0700

    v0.0.12

--- ts/src/main.ts
+++ ts/src/main.ts
@@ -2,8 +2,9 @@ import mainBlock from "main.block";
 import gaman from "gaman";
 
 gaman.serv({
-  blocks: [mainBlock],
+  blocks: [mainBlock], // your blocks
   server: {
-    port: 3431,
+    port: 3431, // opsional
+    host: '0.0.0.0' // opsional
   },
 });
